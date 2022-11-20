f6c165caec0eae8a064a94ae518a9ee538a91388
John Alvarez <johnalvarezwork@gmail.com>
2022-11-20 15:14:28 -0600

    pre onDragEnd code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
@@ -8,53 +8,102 @@ import {
   Grid,
   Paper,
 } from '@mui/material';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import Chance from 'chance';
+
+const chance = new Chance();
+
+const arr = [
+  chance.guid(),
+  chance.guid(),
+  chance.guid(),
+  chance.guid(),
+  chance.guid(),
+];
+
+const Item = ({ i, data }) => (
+  <Draggable draggableId={`${data}`} index={i}>
+    {(provided) => (
+      <Grid
+        item
+        xs={12}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+      >
+        <Paper elevation={8}>
+          <Typography color="text.secondary" gutterBottom>
+            Task {i + 1}
+          </Typography>
+        </Paper>
+      </Grid>
+    )}
+  </Draggable>
+);
+
+const DropItem = () => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+
+  return (
+    <Droppable droppableId="droppable-1" type="PERSON">
+      {(provided) => (
+        <Grid
+          container
+          spacing={2}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          {arr.map((ele, i) => (
+            <Item key={`${ele}-key`} data={ele} i={i} />
+          ))}
+          {provided.placeholder}
+        </Grid>
+      )}
+    </Droppable>
+  );
+};
 
 function App() {
   const onDragEnd = (result) => {
     // TODO: reorder our column
   };
-
-  const getListItems = (count) => {
-    const list: any[] = [];
-    for (let i = 0; i < count; i++) {
-      list.push(
-        <Grid item xs={12}>
-          <Paper elevation={8}>
-            <Typography color="text.secondary" gutterBottom>
-              Task {i + 1}
-            </Typography>
-          </Paper>
-        </Grid>
-      );
-    }
-    return <>{list}</>;
-  };
-
   return (
-    <div className="App">
-      <Container>
-        <Card
-          variant="elevation"
-          elevation={10}
-          sx={{
-            margin: '3rem auto',
-            background: 'gray',
-            maxWidth: '25rem',
-            paddingTop: '1rem',
-          }}
-        >
-          <Typography variant="h4" color="text.secondary" gutterBottom>
-            Task List
-          </Typography>
-          <CardContent>
-            <Grid container spacing={2}>
-              {getListItems(4)}
-            </Grid>
-          </CardContent>
-        </Card>
-      </Container>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <Container>
+          <Card
+            variant="elevation"
+            elevation={10}
+            sx={{
+              margin: '3rem auto',
+              background: 'gray',
+              maxWidth: '25rem',
+              paddingTop: '1rem',
+            }}
+          >
+            <Typography variant="h4" color="text.secondary" gutterBottom>
+              Task List
+            </Typography>
+            <CardContent>
+              <DropItem />
+            </CardContent>
+          </Card>
+        </Container>
+      </div>
+    </DragDropContext>
   );
 }
 
